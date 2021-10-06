0c28e8c03f820eeeaa7d958763e9ffd686a55c26
yl-test1 <yehudilara@hotmail.com>
2021-10-06 17:26:15 -0300

    mejora

--- src/trabajoformativo_2/TrabajoFormativo_2.java
+++ src/trabajoformativo_2/TrabajoFormativo_2.java
@@ -24,61 +24,43 @@ public class TrabajoFormativo_2 {
             
             List<String> InstaLista = new ArrayList<>();
             List<String> EstadoLista = new ArrayList<>();
-            //Instagram insta1 = new Instagram();
             System.out.println("Bienvenido a la Red Social");
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Escriba su nuevo usuario");
-            String usuario = sc.nextLine();
-            insta1.setUsuario(usuario);
-            System.out.println("Bienvenido " + insta1.getUsuario());
-            System.out.println("Elija la red social a la que deseas entrar: \n 1.instagram \n 2.Twitter \n 4. Salir");
             
             Scanner sn = new Scanner(System.in);
             boolean salir = false;
-            int opcion; //Guardaremos la opcion del usuario
-
-//            while (!salir) {
-//
-//                try {
-//
-//                    System.out.println("Escribe una de las opciones");
-//                    opcion = sn.nextInt();
-//
-//                    switch (opcion) {
-//                        case 1:
-//                            System.out.println("Has seleccionado la opcion 1, Escriba la Historia que desea subir:");
-//                            String story = sc.next();
-//                            InstaLista.add(story);
-//                            System.out.println("Usted ha subido la siguiente historia con exito:" + InstaLista);
-//                            break;
-//                        case 2:
-//                            System.out.println("Has seleccionado la opcion 2");
-//                            break;
-//                        case 3:
-//                            System.out.println("Has seleccionado la opcion 3");
-//                            break;
-//                        case 4:
-//                            salir = true;
-//                            break;
-//                        default:
-//                            System.out.println("Solo números entre 1 y 4");
-//                    }
-//                } catch (InputMismatchException e) {
-//                    System.out.println("Debes insertar un número");
-//                    sn.next();
-//                }
-//            }
+            boolean salir2 = false;
+            int opcion;
+            
             while(!salir){
-            System.out.println("Escribe una de las opciones");
+            System.out.println("Elija la red social a la que deseas entrar: \n 1.instagram \n 2.Twitter \n 5. Salir");
             opcion = sn.nextInt();
                 if (opcion == 1) {
-                    System.out.println("Has seleccionado la opcion 1, Escriba la Historia que desea subir:");
-                    String story = sc.next();
-                    InstaLista.add(story);
-                    System.out.println("Usted ha subido la siguiente historia con exito:" + InstaLista);
-                    }
-            
+                    System.out.println("Has seleccionado la opcion 1");
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Escriba su nuevo usuario");
+                    String usuario = sc.nextLine();
+                    insta1.setUsuario(usuario);
+                    System.out.println("Bienvenido " + insta1.getUsuario());
+                        while(!salir2){
+                            System.out.println("1.Subir una historia 2.Subir un post 3.volver al home");
+                        opcion = sn.nextInt();
+                            if (opcion == 1) {
+                            System.out.println("Escriba historia a subir");
+                            String story = sn.next();
+                            InstaLista.add(story);
+                            System.out.println("Usted ha subido la siguiente historia con exito:" + InstaLista);
+                            }
+                            if (opcion == 3) {
+                                salir2=true;
+                            }
+                        }
+                    }//final-if-1
+                if (opcion == 5) {
+                    salir=true;
+                }
+
             }
+
             
             System.out.println("Muchas gracias por preferir Red Social");
 
